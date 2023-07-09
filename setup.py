ae2e2e15e5a7b0f83f19ce839db32b8226ede43d
Rahul-Kumar-Paswan <rahulkumarpaswan941+auropro@gmail.com>
2023-07-09 12:25:02 +0530

    added version 4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="project_package",
-    version="1.2.1",
+    version='1.2.1',
     packages=find_packages(),
     description='A simple Python package',
     author='Sweet Heart',
